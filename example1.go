4f60fe184c4cf72e177c6861da7127f7b23f5e5c
سبحان شفیعی <shafiee-sobhan@favarazavi.ir>
2023-08-13 08:29:08 +0330

    example1 is done

---
+++ example1.go
@@ -0,0 +1,19 @@
+package main
+
+//func main() {
+//
+//	now := time.Now()
+//	defer func() {
+//		fmt.Println(time.Since(now))
+//	}()
+//
+//	ninjas := []string{"sobhan", "amir", "soroush", "asma", "taraneh"}
+//
+//	for _, ninja := range ninjas {
+//		attacker(ninja)
+//	}
+//}
+//
+//func attacker(name string) {
+//	fmt.Println("we need to attack to ", name)
+//}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module concurrent-example
+
+go 1.19
